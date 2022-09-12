4bbd79040eacb1097d4854e038b30a19c399c971
sojith <sojith@gmail.com>
2022-09-12 17:08:09 +0530

    Pause and Stop Button added

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ import ffmpeg
 import wave
 import pyaudio
 import os
+import threading
 
 def selectButtonFunc():
 	global playlist_pos_y
@@ -38,7 +39,8 @@ def movePositionFunc(movement):
 	global current_pos
 	global playlist_files
 	global playlist_file_pos
-	if len(playlist_files) >= 1:
+	global playStatus
+	if len(playlist_files) >= 1 and playStatus == 0:
 		if current_pos < (len(playlist_files)-1) and movement == 1:
 			current_pos = current_pos + 1
 			canvas.itemconfig(playlist_file_pos[current_pos], fill="green3")
@@ -50,6 +52,8 @@ def movePositionFunc(movement):
 
 
 def playFile(filename):
+	global playStatus
+	global pauseStatus
 	try:
 		os.remove("test2.wav")
 	except:
@@ -69,21 +73,52 @@ def playFile(filename):
 
 	data = wf.readframes(WIDTH)
 
-	while len(data):
-	    stream.write(data)
-	    data = wf.readframes(WIDTH)
+	playStatus = 1
+	pauseStatus = 0
 
+	print("Pause Status ", pauseStatus)
+	while len(data) and playStatus == 1:
+		if pauseStatus == 0:
+		    stream.write(data)
+		    data = wf.readframes(WIDTH)
+		else:
+			pass
 	stream.stop_stream()
 	stream.close()
 
 	p.terminate()
+	playStatus = 0		# i.e. nothing is playing anymore
+	pauseStatus = 0		# i.e. nothing is paused anymore
 	print("MP3 over")
+
+def playBtnClicked(filename):
+	if filename and playStatus == 0:
+		t1 = threading.Thread(target=playFile, args=(filename,))
+		t1.start()
+
+def stopBtnClicked():
+	global playStatus
+	playStatus = 0
+	pauseBtn.configure(background="dim gray", activebackground="slate gray")
+
+def pauseBtnClicked():
+	global pauseStatus
+	global playStatus
+	if playStatus == 1 and pauseStatus == 0:
+		pauseStatus = 1
+		pauseBtn.configure(background="dark slate blue", activebackground="slate blue")
+	elif playStatus == 1 and pauseStatus == 1:
+		pauseStatus = 0
+		pauseBtn.configure(background="dim gray", activebackground="slate gray")
+
 
 
 playlist_pos_y = 50	#coordinates of each file inthe playlist
 playlist_files = []	#names of files in the playlist
 playlist_file_pos = []	#position assigned to each file in the playlist
 current_pos = 0		#position of the currently selectd file inthe playlist
+playStatus = 0 		#Current of Play. 0 is not playing. 1 is playing
+pauseStatus = 0		#Current status of Pause. 1 is Paused
 wind = tkinter.Tk()
 wind.geometry("420x350")
 wind.resizable(False,False)
@@ -106,6 +141,12 @@ previousTrackImg.configure(width=32, height=32)
 playImg=tkinter.PhotoImage(file='./icons/play.png')
 playImg.configure(width=32, height=32)
 
+stopImg=tkinter.PhotoImage(file='./icons/stop.png')
+stopImg.configure(width=32, height=32)
+
+pauseImg=tkinter.PhotoImage(file='./icons/pause.png')
+pauseImg.configure(width=32, height=32)
+
 
 
 #Select Button
@@ -119,13 +160,22 @@ backwardBtn = tkinter.Button(canvas, image=previousTrackImg, background="dim gra
 backwardBtn.place(x=50, y=305)
 
 #Play
-playBtn = tkinter.Button(canvas, image=playImg, background="dim gray", activebackground="slate gray", highlightthickness=0, command=lambda: playFile(playlist_files[current_pos]))
+#playBtn = tkinter.Button(canvas, image=playImg, background="dim gray", activebackground="slate gray", highlightthickness=0, command=lambda: playFile(playlist_files[current_pos]))
+playBtn = tkinter.Button(canvas, image=playImg, background="dim gray", activebackground="slate gray", highlightthickness=0, command=lambda: playBtnClicked(playlist_files[current_pos]))
 playBtn.place(x=90, y=305)
 
 #Forward
 #nextTrackBtn = tkinter.Button(canvas, text="Forward", background="dim gray", activebackground="slate gray", highlightthickness=0, command=lambda: movePositionFunc(1))
 previousTrackBtn = tkinter.Button(canvas, image=nextTrackImg, background="dim gray", activebackground="slate gray", highlightthickness=0, command=lambda: movePositionFunc(1))
 previousTrackBtn.place(x=130, y=305)
+
+#Stop
+stopBtn = tkinter.Button(canvas, image=stopImg, background="dim gray", activebackground="slate gray", highlightthickness=0, command=stopBtnClicked)
+stopBtn.place(x=170, y=305)
+
+#Pause
+pauseBtn = tkinter.Button(canvas, image=pauseImg, background="dim gray", activebackground="slate gray", highlightthickness=0, command=pauseBtnClicked)
+pauseBtn.place(x=210, y=305)
 
 #btn.pack()
 
